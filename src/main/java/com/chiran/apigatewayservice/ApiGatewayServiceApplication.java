2e94d0a3950325f547438a3beb1318de3d2071fc
Chiran_105626 <Chiran_105626@105626-002L>
2022-12-11 14:58:57 +0530

    init

---
+++ src/main/java/com/chiran/apigatewayservice/ApiGatewayServiceApplication.java
@@ -0,0 +1,16 @@
+package com.chiran.apigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.context.annotation.Bean;
+
+import java.beans.Customizer;
+
+@SpringBootApplication
+public class ApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServiceApplication.class, args);
+	}
+}
