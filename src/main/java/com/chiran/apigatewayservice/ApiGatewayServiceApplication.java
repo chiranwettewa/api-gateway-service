886d2d5cf83ed9a18fde118db231d2b66958af36
chiranwettewa <chiranwettewa@gmail.com>
2022-12-14 17:08:00 +0530

    init

--- src/main/java/com/chiran/apigatewayservice/ApiGatewayServiceApplication.java
+++ src/main/java/com/chiran/apigatewayservice/ApiGatewayServiceApplication.java
@@ -11,7 +11,6 @@ import java.beans.Customizer;
 
 @SpringBootApplication
 public class ApiGatewayServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayServiceApplication.class, args);
 	}
